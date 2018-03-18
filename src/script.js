8b96fa321d3b8768ac3ec5b9955220107fe03ec7
Jarosław Goszowski <goszowski@gmail.com>
2018-03-18 13:21:19 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,27 @@
 Date.prototype.julianToGregorian = function() {
-  return new Date(this.valueOf()).setDate(date.getDate() + 14);
-}
+			var date = new Date(this.valueOf());
+			var addDays = 0;
+
+			if(date >= new Date('1582-10-05') && date < new Date('1700-02-29')) {
+				addDays = 10;
+			}
+			else if(date >= new Date('1700-03-01') && date < new Date('1800-02-29')) {
+				addDays = 11;
+			}
+			else if(date >= new Date('1800-03-01') && date < new Date('1900-02-29')) {
+				addDays = 12;
+			}
+			else if(date >= new Date('1900-03-01') && date < new Date('2100-02-29')) {
+				addDays = 13;
+			}
+			else if(date >= new Date('2100-03-01') && date < new Date('2200-02-29')) {
+				addDays = 14;
+			}
+			else if(date >= new Date('2200-03-01') && date < new Date('2300-02-29')) {
+				addDays = 15;
+			}
+
+			date.setDate(date.getDate() + addDays);
+
+			return date;
+		}
